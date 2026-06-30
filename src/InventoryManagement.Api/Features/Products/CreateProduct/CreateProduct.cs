68537679ba54919b2c17e731ec158e3aca3feceb
anlucys <pregoadisaputroo@gmail.com>
2026-07-01 01:31:47 +0700

    make a create product endpoint

---
+++ src/InventoryManagement.Api/Features/Products/CreateProduct/CreateProduct.cs
@@ -0,0 +1,60 @@
+using InventoryManagement.Api.Data;
+using InventoryManagement.Api.Data.Entity;
+using InventoryManagement.Api.Features.Products.Constant;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Api.Features.Products.CreateProduct;
+
+public static class CreateProduct
+{
+    public static void MapCreateProduct(this IEndpointRouteBuilder group)
+    {
+        group.MapPost(
+            "/",
+            async (CreateProductRequest request, AppDbContext db) =>
+            {
+                var isProductExist = await db.Products.AnyAsync(p =>
+                    EF.Functions.ILike(p.Name, request.Name)
+                    || EF.Functions.ILike(p.Sku, request.Sku)
+                );
+
+                if (isProductExist)
+                {
+                    return Results.BadRequest(
+                        $"Product with Name {request.Name} or SKU {request.Sku} already exist."
+                    );
+                }
+
+                var newProduct = new Product
+                {
+                    Name = request.Name,
+                    Sku = request.Sku,
+                    Description = request.Description
+                    Price = request.Price,
+                    Stock = request.Stock,
+                    MinimumStock = request.MinimumStock,
+                    CategoryId = request.CategoryId
+                };
+
+                db.Products.Add(newProduct);
+                await db.SaveChangesAsync();
+
+                return Results.CreatedAtRoute(
+                    ProductEndpointNames.GetProductById,
+                    new { id = newProduct.Id },
+                    new CreateProductResponse(
+                       newProduct.Id,
+                       newProduct.Name,
+                       newProduct.Sku,
+                       newProduct.Description,
+                       newProduct.Price,
+                       newProduct.Stock,
+                       newProduct.MinimumStock,
+                       newProduct.CategoryId,
+                       newProduct.Category.Name,
+                       newProduct.CreatedAt,
+                       newProduct.UpdatedAt));
+            }
+        );
+    }
+}
