54faff33ba5534c2d6233afdfcc5c6c3d2a4efca
anlucys <pregoadisaputroo@gmail.com>
2026-07-15 01:20:41 +0700

    helper for fetching the api

---
+++ src/InventoryManagement.Client/src/lib/server/api.js
@@ -0,0 +1,23 @@
+import { error } from '@sveltejs/kit';
+import { API_URL } from '$lib/api/apiUrl';
+
+export async function serverApi(path, cookies, options = {}) {
+	const token = cookies.get('token');
+
+	console.log('token:', token);
+
+	const response = await fetch(`${API_URL}${path}`, {
+		...options,
+		headers: {
+			'Content-Type': 'application/json',
+			...(token && { Authorization: `Bearer ${token}` }),
+			...options.headers
+		}
+	});
+
+	if (!response.ok) {
+		throw error(response.status, await response.text());
+	}
+
+	return response.json();
+}
