f652473e12cfcfcf5bda4a6bfda69815f762c5d2
anlucys <pregoadisaputroo@gmail.com>
2026-07-14 17:02:46 +0700

    make a login routes page

---
+++ src/InventoryManagement.Client/src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { login } from '$lib/api/auth';
+
+	let username = $state('');
+	let password = $state('');
+
+	async function handleLogin() {
+		try {
+			const response = await login(username, password);
+
+			console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+</script>
+
+<input bind:value={username} placeholder="username" />
+
+<input type="password" bind:value={password} placeholder="password" />
+
+<button onclick={handleLogin}>login</button>
