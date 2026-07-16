2935d101a67632a061cd8931f13e73a51332a329
anlucys <pregoadisaputroo@gmail.com>
2026-07-16 16:27:31 +0700

    login button only when not log in

--- src/InventoryManagement.Client/src/routes/+page.svelte
+++ src/InventoryManagement.Client/src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	import { goto } from '$app/navigation';
+	let { data } = $props();
 </script>
 
 <h1>Inventory Management</h1>
 
-<button onclick={() => goto('/auth/login')}>login</button>
+{#if !data.user}
+	<button onclick={() => goto('/auth/login')}>login</button>
+{/if}
