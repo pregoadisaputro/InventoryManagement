b83c9bf5b0ea215e5b3394e03e80a3e08eddca5a
anlucys <pregoadisaputroo@gmail.com>
2026-07-24 02:08:32 +0700

    remove port

--- src/InventoryManagement.Api/Program.cs
+++ src/InventoryManagement.Api/Program.cs
@@ -19,9 +19,6 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
 builder.Services.AddProblemDetails().AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
